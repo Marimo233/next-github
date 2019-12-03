a544b6eb4e6bdc8d60a5942dae35dab79e718a14
marimo <1072452597>
2019-12-03 22:54:12 +0800

    feat:接入koa-session和redis

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const Redis=require('ioredis')
 const dev=process.env.Node_ENV!=='production'
 const app=new Next({dev})
 const handle=app.getRequestHandler()
+const session=require('koa-session')
+const RedisSessionStore=require('./server/session-store')
 app.prepare().then(()=>{
     const server=new Koa()
     const router=new Router()
-    // const redis=new Redis()
+    const redis=new Redis()
+    //加密Key
+    server.keys=["Marimo's next-github app"]
+    //当设置signed为true时用keys来加密cookie
+    const SESSION_CONFIG={
+      key:'marimo',
+      store:new RedisSessionStore(redis)
+    }
+    server.use(session(SESSION_CONFIG,server))
     router.get("/a/:id",async (ctx,next)=>{
 			const id=ctx.params.id
         await handle(ctx.req,ctx.res,{
@@ -21,7 +31,7 @@ app.prepare().then(()=>{
     server.use(router.routes())
     server.use(async (ctx, next) => {
       await handle(ctx.req, ctx.res)
-      // ctx.respond = false 暂时注释掉
+      ctx.respond = false 
     })
     server.listen(3000,()=>{
         console.log('sucess')
